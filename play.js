e41fe358bc0fe6df026286b62f53880b15b3222a
t-iffany <tiffanyxleong@gmail.com>
2022-11-23 21:53:28 +0000

    Setup stdin by creating setupInput and handleUserInput functions in play.js. Game now terminates using ctrlC.

--- play.js
+++ play.js
@@ -1,5 +1,31 @@
 const net = require("net");
+const { stdin } = require("process");
 
 // Import connect function from client.js
 const {connect} = require("./client");
 
+// setup interface to handle user input from stdin
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  // register an event listener for stdin, which uses fcn handleUserInput that runs when you receive input from your keyboard
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+// in handleUserInput fcn, specify what happens when "data:" is received from stdin
+// what happens when a particular key is pressed on the keyboard input
+const handleUserInput = function () {
+  // your code here
+  stdin.on("data", (key) => {
+    // \u0003 maps to ctrl+c input   Add within the data callback
+    if (key === '\u0003') {
+      process.exit();
+}
+  });
+};
+
+handleUserInput();
