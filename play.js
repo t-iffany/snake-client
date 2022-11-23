ca8f0b9554d751b3eb60640439e914d971f07dd5
t-iffany <tiffanyxleong@gmail.com>
2022-11-23 20:37:38 +0000

    Separated TCP Connection to different module client.js. Exported connect function from client.js and imported into play.js.

--- play.js
+++ play.js
@@ -1,21 +1,5 @@
 const net = require("net");
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: '165.227.47.243', // IP address here,
-    port: 50541 // PORT number here,
-  });
+// Import connect function from client.js
+const {connect} = require("./client");
 
-  conn.on("data", (data) => {
-    console.log("Server says: ", data);
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  return conn;
-};
-
-console.log("Connecting ...");
-connect();
